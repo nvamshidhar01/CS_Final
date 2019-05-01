08327de7d7220575579cdf69a52601629f502710
JacobL <jacob.lemley@utexas.edu>
2019-04-30 20:55:29 -0500

    changed up the file reading and room classes, as well as restructured the default file format

--- Himalayas.py
+++ Himalayas.py
@@ -20,18 +20,41 @@ class Player: #class to store player data, like inventory, location, etc.
 
 class Room:
     def __init__(self, name):
-        boundary = ["INITIAL","NORMAL","LOCATIONS","OBJECTS"]
         self.name = name
         self.filepath = name + ".txt"
-        #separate room default values file into class attributes
+        self.objects = []
+        self.locations = []
+        self.initialDescription = ""
+        self.normalDescription = ""
+        #read defaults from room file
         file = open(self.filepath, "r")
-        raw = file.readlines()
-        final = []
-        for line in raw:
+        raw = []
+        for line in file:
             line = line.rstrip("\n")
-            final.append(line)
+            raw.append(line)
+        file.close()
 
-        print(final)
+        self.title = raw[0]
+        # set of while and for loops to sort default room data into appropriate attributes, allows for varied numbers of locations and objects
+        n = 2
+        while raw[n] != "NORMAL": #set initial description attribute, should copy exactly what you put in INITIAL area in the txt file
+            self.initialDescription = self.initialDescription + str(raw[n]) + "\n"
+            n += 1
+        n+=1
+        while raw[n] != "LOCATIONS": #set normal description attribute, should copy exactly what you put in NORMAL area in the txt file
+            self.normalDescription = self.normalDescription + str(raw[n]) + "\n"
+            n+=1
+        n+=1
+        while raw[n] != "OBJECTS": # set list of locations available
+            self.locations.append(raw[n])
+            n+=1
+        n+=1
+        for i in (raw[n:]): #set list of objects (formatted as a list of lists containing an object and description)
+            temp = []
+            temp = i.split(":")
+            self.objects.append(temp)
+
+
 
         ''' OLDER VERSION - IGNORE - FOR REFERENCE
         #dictionary containing naming and default values for each room
@@ -60,14 +83,29 @@ class Room:
         for i in range(n,len(self.defaults)):
             self.locations.append(self.defaults[n])
         '''
-    def roomDescription(self, player):         #print out room description
-        for n in range(len(self.objects)):
-            if not any(self.objects[n] in s for s in player.inventory):
-                print(self.objects[n])
+    def roomDescription(self, player):
+        avItems = [] #empties list of available items in the room
+        objstr = "There is a "
+        print(self.normalDescription)
+        for i in self.objects:
+            if self.objects.index(i) != len(self.objects)-1:
+                objstr = objstr + i[0] + ", "
+            else:
+                objstr = objstr + "and " + i[0] + " in here."
+        if len(self.objects) > 0 :
+            print(objstr)
+
 
 def main():
     player1 = Player()
     signroom = Room("signroom")
     river = Room("river")
-
+    '''
+    print(signroom.title)
+    print(signroom.initialDescription)
+    print(signroom.normalDescription)
+    print(signroom.locations)
+    print(signroom.objects)
+    '''
+    signroom.roomDescription(player1)
 main()
